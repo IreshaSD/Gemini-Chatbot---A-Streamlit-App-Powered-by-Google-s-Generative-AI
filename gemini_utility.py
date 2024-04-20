6cf2c52c45cb1c511f95802db4e78505dba2ccf1
Iresha Sandamali <152151059+IreshaSD@users.noreply.github.com>
2024-04-20 22:34:43 +0300

    Update gemini_utility.py

--- gemini_utility.py
+++ gemini_utility.py
@@ -4,13 +4,13 @@ import json
 
 import google.generativeai as genai
 
-working_directory = os.path.dirname(os.path.abspath(
-    __file__))  # os.path.abspath(__file__): This function returns the absolute path of the current Python script file without resolving symbolic links. It simply resolves any relative paths and returns the absolute path as it appears in the filesystem.
+# Working directory path
+working_directory = os.path.dirname(os.path.abspath(__file__))  # os.path.abspath(__file__): This function returns the absolute path of the current Python script file without resolving symbolic links. It simply resolves any relative paths and returns the absolute path as it appears in the filesystem.
 
-
+# path of config_data file 
 config_file_path = f"{working_directory}/config.json"
-config_data = json.load(open(config_file_path))
-# print(config_data) # this will give the key value pair
+config_data = json.load(open("config.json"))
+
 
 # Loading the API key
 GOOGLE_API_KEY = config_data["GOOGLE_API_KEY"]
@@ -22,15 +22,13 @@ genai.configure(api_key=GOOGLE_API_KEY)
 
 # function to load gemini-pro-model for chatbot
 def load_gemini_pro_model():
-    gemini_pro_model = genai.GenerativeModel(
-        "gemini-pro")  # gemini pro takes input as text and it will give output as text also
+    gemini_pro_model = genai.GenerativeModel("gemini-pro")  # gemini pro takes input as text and it will give output as text also
     return gemini_pro_model
 
 
 # Function for image captioning
 def gemini_pro_vision_response(prompt, image):
-    gemini_pro_vision_modl = genai.GenerativeModel(
-        "gemini-pro-vision")  # gemini pro vision is a multimodel. It takes  text and as well as images as inputs but it gives the  output as text.
+    gemini_pro_vision_modl = genai.GenerativeModel("gemini-pro-vision")  # gemini pro vision is a multimodel. It takes  text and as well as images as inputs but it gives the  output as text.
     response = gemini_pro_vision_modl.generate_content([prompt, image])
     result = response.text
     return result
