b1814e8e4e1b687e82428d5717006d821ca9fe4e
Iresha Sandamali <152151059+IreshaSD@users.noreply.github.com>
2024-04-20 22:45:12 +0300

    Update gemini_utility.py

--- gemini_utility.py
+++ gemini_utility.py
@@ -1,49 +1,38 @@
-import streamlit
 import os
 import json
+from PIL import Image
 
 import google.generativeai as genai
 
-# Working directory path
-working_directory = os.path.dirname(os.path.abspath(__file__))  # os.path.abspath(__file__): This function returns the absolute path of the current Python script file without resolving symbolic links. It simply resolves any relative paths and returns the absolute path as it appears in the filesystem.
+# working directory path
+working_dir = os.path.dirname(os.path.abspath(__file__))
 
-# path of config_data file 
-config_file_path = f"{working_directory}/config.json"
+# path of config_data file
+config_file_path = f"{working_dir}/config.json"
 config_data = json.load(open("config.json"))
 
-
-# Loading the API key
+# loading the GOOGLE_API_KEY
 GOOGLE_API_KEY = config_data["GOOGLE_API_KEY"]
-
 
 # configuring google.generativeai with API key
 genai.configure(api_key=GOOGLE_API_KEY)
 
 
-# function to load gemini-pro-model for chatbot
 def load_gemini_pro_model():
-    gemini_pro_model = genai.GenerativeModel("gemini-pro")  # gemini pro takes input as text and it will give output as text also
+    gemini_pro_model = genai.GenerativeModel("gemini-pro")
     return gemini_pro_model
 
 
-# Function for image captioning
+# get response from Gemini-Pro-Vision model - image/text to text
 def gemini_pro_vision_response(prompt, image):
-    gemini_pro_vision_modl = genai.GenerativeModel("gemini-pro-vision")  # gemini pro vision is a multimodel. It takes  text and as well as images as inputs but it gives the  output as text.
-    response = gemini_pro_vision_modl.generate_content([prompt, image])
+    gemini_pro_vision_model = genai.GenerativeModel("gemini-pro-vision")
+    response = gemini_pro_vision_model.generate_content([prompt, image])
     result = response.text
     return result
 
 
-# Image loading part for testing
-# from PIL import Image
-# image = Image.open("nature.jpg")
-# prompt = "Write a BRIEF caption for this image"
-# output = gemini_pro_vision_response(prompt,image)
-# print(output)
-
-# Similarly Embeding will take the input as text and returns embedding
-# Funnction to get embedings for text
-def embedding_model_response(input_text):
+# get response from embeddings model - text to embeddings
+def embeddings_model_response(input_text):
     embedding_model = "models/embedding-001"
     embedding = genai.embed_content(model=embedding_model,
                                     content=input_text,
@@ -52,13 +41,13 @@ def embedding_model_response(input_text):
     return embedding_list
 
 
-
-
-
-# Function to get a response from gemini-pro LLM
+# get response from Gemini-Pro model - text to text
 def gemini_pro_response(user_prompt):
     gemini_pro_model = genai.GenerativeModel("gemini-pro")
     response = gemini_pro_model.generate_content(user_prompt)
     result = response.text
     return result
+
+
+
 
