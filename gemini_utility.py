501e83af5cda0ea4f2ccb2a6c174ce094a904998
Iresha Sandamali <152151059+IreshaSD@users.noreply.github.com>
2024-04-27 09:44:19 +0300

    Update gemini_utility.py

--- gemini_utility.py
+++ gemini_utility.py
@@ -1,21 +1,32 @@
 import os
 import json
-from PIL import Image
-
 import google.generativeai as genai
 
-# working directory path
-working_dir = os.path.dirname(os.path.abspath(__file__))
+from PIL import Image
+from dotenv import load_dotenv
 
-# path of config_data file
-config_file_path = f"{working_dir}/config.json"
-config_data = json.load(open("config.json"))
 
-# loading the GOOGLE_API_KEY
-GOOGLE_API_KEY = config_data["GOOGLE_API_KEY"]
 
-# configuring google.generativeai with API key
+# # working directory path
+# working_dir = os.path.dirname(os.path.abspath(__file__))
+
+# # path of config_data file
+# config_file_path = f"{working_dir}/config.json"
+# config_data = json.load(open("config.json"))
+
+# # loading the GOOGLE_API_KEY
+# GOOGLE_API_KEY = config_data["GOOGLE_API_KEY"]
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Access the API key from environment variables
+GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
+
+# Configure google.generativeai with API key
 genai.configure(api_key=GOOGLE_API_KEY)
+
+
 
 
 def load_gemini_pro_model():
